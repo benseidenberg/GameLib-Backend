00017d0ecb9fa513bc61058a30a5f5683aa6b0b7
benseidenberg <44117044+benseidenberg@users.noreply.github.com>
2025-12-08 15:16:50 -0500

    slightly buffed batch size
    increased batch size since it can handle it now

--- gamelib-backend/src/db/repositories/games_db.py
+++ gamelib-backend/src/db/repositories/games_db.py
@@ -266,7 +266,7 @@ class GamesRepository:
         min_negative: Optional[int] = None,
         order_by: str = 'positive',
         ascending: bool = False,
-        batch_size: int = 75,
+        batch_size: int = 100,
         stop_limit: Optional[int] = None,
         apply_content_filter: bool = True,
     ) -> List['Game']:
